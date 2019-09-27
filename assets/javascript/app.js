ca5ee03a42bc7b856ff71bb234aa7883cf38da1a
Dohn Joe <johnphtdo@gmail.com>
2019-09-27 11:06:38 -0500

    Got correct and incorrect counter to work properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
-
-    $(document).on("click","#doneBtn", function(){
-        resultsPage()
-    
-    })
+  $(document).on("click", "#doneBtn", function() {
+    resultsPage();
+  });
 
   // When Start game is clicked, hide the start button.
   $(`#startBtn`).on("click", function() {
     $(`#startBtn`).remove();
 
     // // Populate a timer
-        subtractTime()
+    subtractTime();
     // Populate the questions
     for (let i = 0; i < questions.length; i++) {
       $(`.startWrap`).append(`<hr><p>` + questions[i].question + `</p>`);
@@ -18,154 +16,164 @@ $(document).ready(function() {
       // Populate the answer choices
       for (let a = 0; a < questions[i].answers.length; a++) {
         $(`.startWrap`).append(
-          "<br><input type='radio' name= 'questions- " +
+          "<br><input type='radio' name= 'radio-'" +
             i +
-            "' value= '" +
+            "' value='" +
             questions[i].answers[a] +
             "'>" +
             questions[i].answers[a]
         );
       }
     }
-    $(`.startWrap`).append(`<hr><button type="button" id="doneBtn">Done</button>`)
-
+    $(`.startWrap`).append(
+      `<hr><button type="button" id="doneBtn">Done</button>`
+    );
   });
-  
+
   var questions = [
-      {
-          question: "To get over Richard, what did Monica start making?",
-          answers: ["Marmalade", "Jam", "Candy", "Cookies"],
-          correctAnswer: "Jam"
-        },
     {
-        question: "What was the name of the self help book that the girls loved?",
-        answers: [
-            "Be Your Own Windkeeper",
-            "Be Your Own Lightning Bearer",
-            "Be Your Own Cleansing Pool",
-            "Be Your Own Person"
-        ],
-        correctAnswer: "Be Your Own Windkeeper"
+      question: "To get over Richard, what did Monica start making?",
+      answers: ["Marmalade", "Jam", "Candy", "Cookies"],
+      correctAnswer: "Jam"
     },
     {
-        question: "What was wrong with the couch Ross returned to the store?",
-        answers: [
-            "The colour was wrong",
-            "It was cut in half",
-            "It had a stain",
-            "It would not PIVOT!"
-        ],
-        correctAnswer: "It was cut in half"
+      question: "What was the name of the self help book that the girls loved?",
+      answers: [
+        "Be Your Own Windkeeper",
+        "Be Your Own Lightning Bearer",
+        "Be Your Own Cleansing Pool",
+        "Be Your Own Person"
+      ],
+      correctAnswer: "Be Your Own Windkeeper"
     },
     {
-        question:
+      question: "What was wrong with the couch Ross returned to the store?",
+      answers: [
+        "The colour was wrong",
+        "It was cut in half",
+        "It had a stain",
+        "It would not PIVOT!"
+      ],
+      correctAnswer: "It was cut in half"
+    },
+    {
+      question:
         "What was Phoebe in charge of at Rachel's surprise birthday party?",
-        answers: [
-            "Balloons and Ice",
-            "Rice and Ice",
-            "Cups and Ice",
-            "Rice and Balloons"
-        ],
-        correctAnswer: "Cups and Ice"
+      answers: [
+        "Balloons and Ice",
+        "Rice and Ice",
+        "Cups and Ice",
+        "Rice and Balloons"
+      ],
+      correctAnswer: "Cups and Ice"
     },
     {
-        question:
+      question:
         "What did Chandler change about his appearance that no one noticed?",
-        answers: ["His Hair", "His Mustache", "His Eyebrows", "His Glasses"],
-        correctAnswer: "His Glasses"
+      answers: ["His Hair", "His Mustache", "His Eyebrows", "His Glasses"],
+      correctAnswer: "His Glasses"
     },
     {
-        question: "What was the name of the coffee shop?",
-        answers: ["Perk Central", "Perks Coffee", "Central Perk", "Coffee Perk"],
-        correctAnswer: "Central Perk"
+      question: "What was the name of the coffee shop?",
+      answers: ["Perk Central", "Perks Coffee", "Central Perk", "Coffee Perk"],
+      correctAnswer: "Central Perk"
     },
     {
-        question: "How many times did Ross get divorced?",
-        answers: ["5", "4", "2", "3"],
-        correctAnswer: "3"
+      question: "How many times did Ross get divorced?",
+      answers: ["5", "4", "2", "3"],
+      correctAnswer: "3"
     },
     {
-        question: "How many kids did Phoebe have?",
-        answers: ["5", "3", "2", "4"],
-        correctAnswer: "3"
+      question: "How many kids did Phoebe have?",
+      answers: ["5", "3", "2", "4"],
+      correctAnswer: "3"
     },
     {
-        question: "What was Joey's iconic pick-up line?",
-        answers: [
-            "How are you doing?",
-            "Have you met Joey?",
-            "How you doin'?",
-            "How are you?"
-        ],
-        correctAnswer: "How you doin'?"
+      question: "What was Joey's iconic pick-up line?",
+      answers: [
+        "How are you doing?",
+        "Have you met Joey?",
+        "How you doin'?",
+        "How are you?"
+      ],
+      correctAnswer: "How you doin'?"
     },
     {
-        question: "What was the name Joey uses as an alias?",
-        answers: ["Ken Adams", "Max Power", "Chandler Bing", "Adams Ken"],
-        correctAnswer: "Ken Adams"
+      question: "What was the name Joey uses as an alias?",
+      answers: ["Ken Adams", "Max Power", "Chandler Bing", "Adams Ken"],
+      correctAnswer: "Ken Adams"
     },
     {
-        question:
+      question:
         "What did Phoebe say was wrong with the Rachel's plane to France?",
-        answers: [
-            "The Right Phalange",
-            "The Left Phalange",
-            "Nothing",
-            "The Front Phalange"
-        ],
-        correctAnswer: "The Left Phalange"
+      answers: [
+        "The Right Phalange",
+        "The Left Phalange",
+        "Nothing",
+        "The Front Phalange"
+      ],
+      correctAnswer: "The Left Phalange"
     },
     {
-        question: "What was Chandler's job before he went into advertising?",
-        answers: [
-            "Data Transponder",
-            "Joey's Butler",
-            "Actor",
-            "Statistical Analysis and Data Reconfiguration"
-        ],
-        correctAnswer: "Statistical Analysis and Data Reconfiguration"
+      question: "What was Chandler's job before he went into advertising?",
+      answers: [
+        "Data Transponder",
+        "Joey's Butler",
+        "Actor",
+        "Statistical Analysis and Data Reconfiguration"
+      ],
+      correctAnswer: "Statistical Analysis and Data Reconfiguration"
     },
     {
-        question: "Where was Joey backpacking in his magical story?",
-        answers: ["Asia", "Canada", "Western Europe", "Africa"],
-        correctAnswer: "Western Europe"
+      question: "Where was Joey backpacking in his magical story?",
+      answers: ["Asia", "Canada", "Western Europe", "Africa"],
+      correctAnswer: "Western Europe"
     },
     {
-        question: "What was Chandler's middle name?",
-        answers: ["Murika", "Marvin", "Muriel", "Miracle"],
-        correctAnswer: "Muriel"
+      question: "What was Chandler's middle name?",
+      answers: ["Murika", "Marvin", "Muriel", "Miracle"],
+      correctAnswer: "Muriel"
     },
     {
-        question: "What made Chandler start to freak out about his wedding?",
-        answers: ["The In-Laws", "The Bings", "Joey", "Marcel"],
-        correctAnswer: "The Bings"
+      question: "What made Chandler start to freak out about his wedding?",
+      answers: ["The In-Laws", "The Bings", "Joey", "Marcel"],
+      correctAnswer: "The Bings"
     }
-];
-// Starting variables
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var unanswered = 0;
-var timer = 120;
-var intervalId;
+  ];
+  // Starting variables
+  var userScore = {
 
+    correct: 0,
+    incorrect:0,
+    unanswered:15,
 
-function subtractTime(){
+  }
+  var timer = 120;
+  var intervalId;
+
+  function subtractTime() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-}
-function decrement(){
-    timer--
+  }
+  function decrement() {
+    timer--;
     $(`.timer`).html(`<hr><p>Time remaining: ` + timer + ` seconds</p>`);
     if (timer === 0) {
-        alert("Times up!")
-        resultsPage()
-}
-}
-function resultsPage(){
-
+      alert("Times up!");
+      resultsPage();
+    }
+  }
+  function resultsPage() {
+    clearInterval(intervalId);
     $(`.timer`).remove();
     $(`.startWrap`).remove();
-    
-}
 
-})
+    if ($('input[name="radio-0"]:checked').val() == questions[0].correctAnswer.val){
+        userScore.correct++;
+        } else{
+            userScore.incorrect++;
+        }
+        console.log(userScore);
+    
+  }
+});
